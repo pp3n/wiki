8e815a84660674f5a4c6fc341d3881e8dd284492
pp3n <pp3n@protonmail.ch>
2016-04-10 22:19:46 -0400

    Full app

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+	before_action :find_article, only: [:show]
+
+	def index
+		@articles = Article.all.order("created_at DESC")
+	end
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+	def show
+	end
+	def new
+		@article = Article.new
+	end
+
+	private
+
+	def find_article
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :content, :area)
+	end
+end
