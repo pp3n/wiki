b6810793bddddbe16f104902567334be496b1131
pp3n <pp3n@protonmail.ch>
2016-04-10 22:51:13 -0400

    upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411023944) do
+ActiveRecord::Schema.define(version: 20160411024357) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.string   "area"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "user_id"
+    t.string   "category_id"
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id"
